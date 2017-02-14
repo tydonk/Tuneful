da2a2bd54b729745fd64c5445b1e1a605b384170
Tydon Kommel <tydonk@Tydons-MacBook-Pro.local>
2017-02-13 22:49:43 -0800

    added 'get_songs' endpoint

--- tuneful/api.py
+++ tuneful/api.py
@@ -11,4 +11,13 @@ from tuneful import app
 from .database import session
 from .utils import upload_path
 
+@app.route("/api/songs", methods=["GET"])
+@decorators.accept("application/json")
+def get_songs():
+    """ Return all songs """
+    # Get the songs from the database
+    songs = session.query(models.Song)
 
+    # Convert song data to JSON and return a response
+    data = json.dumps([song.as_dict() for song in songs])
+    return Response(data, 200, mimetype="application/json")
